3bb7732752fef1d5e6747d0ddd69de85768dd6ee
Muhammad Usman <muhammad.u@royalcyber.com>
2021-12-31 14:29:50 +0500

    Blockchain application completed

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Loader } from './loader/Loader';
+export { default as Footer } from './footer/Footer';
+export { default as Navbar } from './navbar/Navbar';
+export { default as Services } from './services/Services';
+export { default as Transactions } from './transactions/Transactions';
+export { default as Welcome } from './welcome/Welcome';
